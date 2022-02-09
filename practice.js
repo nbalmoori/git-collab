7dd694b2a5702335c695635b816b3278fc033dc7
Nikhila Balmoori <nbalmoori@gmail.com>
2022-02-09 09:56:05 -0700

    Update for Emma's changes

--- practice.js
+++ practice.js
@@ -1,8 +1,8 @@
 var exercise1 = "running";
 console.log(exercise1);
 
-var exercises2 = "lifting";
+var exercise2 = "lifting";
 console.log(exercises2)
 
-var exercises3 = "swimming";
-console.log(swimming);
+var exercise3 = "swimming";
+console.log(exercise3);
